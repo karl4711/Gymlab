8ababce3d13955b61fc323d8421fd7b7919d7b20
Bowen Sun <nekomiya6772@gmail.com>
2017-06-08 21:46:12 +0100

    Add valid actions filter(can only select from valid actions when receive specified engagements).

--- qlearning.py
+++ qlearning.py
@@ -2,51 +2,61 @@
 import gym
 import numpy as np
 
-env = gym.make('TestEnv-v0')
-alpha = 0.5 # learning rate
+env_wrapper = gym.make('TestEnv-v0')
+alpha = 0.2 # learning rate
 gamma = 0.99 # discounting factor
 epsilon = 0.2 # exploration rate
 
 action_space_translate = {0:"show ad", 1:"send award", 2:"increase difficulty", 3:"decrease difficulty"}
 
 # initialise Q with ones
-Q = np.ones((env.action_space.n, env.observation_space.size))
+Q = np.ones((env_wrapper.action_space.n, env_wrapper.observation_space.size))
 print("initialise Q with ones: ", Q)
 
-def greedy(Q, state):
-    values = Q[:, state]
+def greedy(valid_actions, state):
+
+    values = Q[valid_actions, state]
+    print("greedy - values:",values)
     candidates = np.argwhere(values == np.amax(values))
-    return np.random.choice(candidates.flatten())
+    print("greedy - candidates:",candidates)
+
+    return np.random.choice(valid_actions[candidates.flatten()])
 
 
-def epsilon_greedy(Q, state, epsilon):
-    values = Q[:, state]
+def epsilon_greedy(valid_actions, state, epsilon):
 
     if np.random.rand() < epsilon:
         print("explore this time..")
-        return np.random.randint(low=0, high=values.shape[0])
+        return np.random.choice(valid_actions)
 
-    return greedy(Q, state)
+    return greedy(valid_actions, state)
 
 
 # qlearning of one episode
 def qlearning():
 
-    obs = env.reset()
-    state = env.observation_space[obs[0],obs[1]]
+    obs = env_wrapper.reset()
+    state = env_wrapper.observation_space[obs[0],obs[1],obs[2],obs[3]]
+
+    # the default first engagement: online
+    engagement = env_wrapper.env.Engagements.online.name
 
     for t in range(0,100):
     
         # choose action with epsilon_greedy
-        action = epsilon_greedy(Q, state, epsilon)
+        # note that there are limit actions to choose according to current engagement
+        action = epsilon_greedy(env_wrapper.env.get_valid_actions(engagement), state, epsilon)
 
-        new_obs, reward, done, _ = env.step(action)
-        new_state = env.observation_space[new_obs[0],new_obs[1]]
+        new_obs, reward, done, info = env_wrapper.step(action)
+        new_state = env_wrapper.observation_space[new_obs[0],new_obs[1],new_obs[2],new_obs[3]]
+
+        # here comes a new engagement after the step
+        engagement = info['engagement']
 
         # update Q
         delta = reward + gamma * np.max(Q[:, new_state]) - Q[action, state]
         Q[action, state] += alpha * delta
 
-        print('update Q[', state, ',', action_space_translate[action],']:',Q[action, state])
+        print('update Q[', state, ',', env_wrapper.env.Actions(action).name,']:',Q[action, state])
 
         state = new_state
--- test_env.py
+++ test_env.py
@@ -4,16 +4,12 @@ import gym
 from gym import spaces
 from gym.utils import seeding
 import numpy as np
-import sqlite3
+from enum import Enum
 
 logger = logging.getLogger(__name__)
 
-event_list = ['ad_closed', 'ad_opened'] #,'offline','mission_start','']
 
-engagement_list = ['online']#,'transaction','mission_complete']
-
-action_space_translate = {0:"show ad", 1:"send award", 2:"increase difficulty", 3:"decrease difficulty"}
-
+        
 
 class TestEnv(gym.Env):
     metadata = {
@@ -21,18 +17,38 @@ class TestEnv(gym.Env):
         'video.frames_per_second': 2
     }
 
+    class Actions(Enum):
+        show_ad = 0
+        send_award = 1
+        increase_difficulty = 2
+        decrease_difficulty = 3
+        goods_recommend = 4
+
+    class Engagements(Enum):
+        online = 0
+        transaction = 1
+        mission_completed = 2
+        mission_failed = 3
+        level_up = 4
+
+    class Events(Enum):
+        ad_closed = 0
+        ad_opened = 1
+        offline = 2
+        mission_start = 3
+
     def __init__(self):
 
         self.threshold = 100
 
-        # 0: show ad, 1: send award, 2: increase difficulty, 3: decrease difficulty 
-        self.action_space = spaces.Discrete(4)
+        # see Actions
+        self.action_space = spaces.Discrete(5)
 
         high = np.array([self.threshold,self.threshold])#, self.threshold, self.threshold, self.threshold])
 
-        # online_motivation(0-99), ad_acceptance(0-99), # mission_skill, consumption_ability
-        x = np.array(np.arange(10000))
-        self.observation_space = np.reshape(x,(100,100))
+        # online_motivation(0-99), ad_acceptance(0-99), mission_skill(0-99), consumption_ability(0-99)
+        x = np.array(np.arange(100000000))
+        self.observation_space = np.reshape(x,(100,100,100,100))
 
         self._seed()
         self.viewer = None
@@ -40,6 +56,8 @@ class TestEnv(gym.Env):
 
         # timestamp of last read event/engagement
         self.last_read_time = 0
+
+        self.engagement_list = list(map(lambda x: x.name, self.Engagements))
 
 
 
@@ -52,7 +70,7 @@ class TestEnv(gym.Env):
     # reset to initial state
     def _reset(self):
         # set fixed initial values
-        self.state = [50, 50]
+        self.state = [50,50,50,50]
 
         print('initial state:', self.state)
 
@@ -61,7 +79,7 @@ class TestEnv(gym.Env):
     # take an action
     def _step(self, action):
 
-        print('send response: ', action_space_translate[action])
+        print('send response: ', self.Actions(action).name)
         # read events after last_read_time and record them
         event = None
         events = {}
@@ -71,7 +89,7 @@ class TestEnv(gym.Env):
 
             event = input('Enter the engagement(online) / event(ad_closed, ad_opened)')
 
-            if event not in engagement_list:
+            if event not in self.engagement_list:
                 print('got event:',event)
                 i += 1
                 events[i] = event #keys can be current time or sth..
@@ -102,7 +120,7 @@ class TestEnv(gym.Env):
         print('new state:', self.state)
 
         # returns: observation (object, next_state), reward (float), done (boolean), info (dict)
-        return np.array(self.state), reward, False, {}
+        return np.array(self.state), reward, False, {"engagement": engagement}
 
 
     # not used currently
@@ -122,4 +140,20 @@ class TestEnv(gym.Env):
         print("state: ", self.state)
 
 
+    # only some of the actions are valid according to current engagement
+    def get_valid_actions(self, engagement):
+        valid_actions = []
+        if engagement not in self.engagement_list:
+            pass
+        elif engagement == self.Engagements.online.name:
+            valid_actions = [self.Actions.send_award.value, self.Actions.show_ad.value, self.Actions.goods_recommend.value]
+        elif engagement == self.Engagements.level_up.name:
+            valid_actions = [self.Actions.send_award.value]
+        elif engagement == self.Engagements.mission_completed.name:
+            valid_actions = [self.Actions.send_award.value, self.Actions.increase_difficulty.value, self.Actions.decrease_difficulty.value]
+        elif engagement == self.Engagements.mission_failed.name:
+            valid_actions = [self.Actions.increase_difficulty.value, self.Actions.decrease_difficulty.value, self.Actions.goods_recommend.value]
+        elif engagement == self.Engagements.transaction.name:
+            valid_actions = [self.Actions.send_award.value, self.Actions.goods_recommend.value]
 
+        return np.array(valid_actions)
